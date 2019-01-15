79d1b8a5c385f4bb302dbb3ea2540bb5e82932e5
Isabelle Krall <isabellekrall@gmail.com>
2019-01-15 11:26:24 +0100

    updates

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import browsersync from 'rollup-plugin-browsersync';
+import postcss from 'rollup-plugin-postcss';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = isProduction === false;
+
+module.exports = {
+  input: 'src/scripts/index.js',
+  output: {
+    file: 'public/giphy.js',
+    format: 'iife'
+  },
+  plugins: [
+    postcss({
+      plugins: []
+    }),
+    (isDevelopment && browsersync({server: 'public'}))
+  ]
+}
